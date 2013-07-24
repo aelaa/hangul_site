26cbb16e701c22d6087ad4a76580797991f42a64
aelaa <aelaau@gmail.com>
2013-07-24 15:10:03 +0400

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HangulSite::Application.config.secret_token = '92b3caabdc30038affa23929c6efe70f7c8f4fcbd18d799845fa619ee3b519f36ab852019aaf16d18784fd56e8679ab95f1ec52998e53dff3607ad34c25446ae'
